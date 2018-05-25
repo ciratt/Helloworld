8ec9a03f841aaff3656e9a8777d8d96bb3a3541b
ciratt <ciro.attanasio@email.cz>
2018-05-25 20:09:10 +0200

    Firt Module MAgento 2

---
+++ Block/Helloworld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ciratt\Helloworld\Block;
+
+class Helloworld extends \Magento\Framework\View\Element\Template {
+
+    public function getHelloWorldTxt() {
+        return 'Hello world!';
+    }
+
+}
