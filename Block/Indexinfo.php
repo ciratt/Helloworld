59722ce69aead6c277b8be074dbe029bb5f5a253
aleromano89 <alx.romano89@gmail.com>
2018-06-01 19:20:09 +0200

    contact form

--- Block/Indexinfo.php
+++ Block/Indexinfo.php
@@ -4,4 +4,13 @@ namespace Ciratt\Helloworld\Block;
 
 class Indexinfo extends \Magento\Framework\View\Element\Template {
 
+    /**
+     * Returns action url for contact form
+     *
+     * @return string
+     */
+    public function getFormAction()
+    {
+        return $this->getUrl('contact/index/post', ['_secure' => true]);
+    }
 }
